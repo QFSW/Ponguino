e8acf08090e9d88bdf2b96ab3801394edc8ed979
Yusuf Ismail <yusuf@qfsw.co.uk>
2024-05-05 18:41:15 +0100

    Helper macros

--- include/utils.h
+++ include/utils.h
@@ -2,6 +2,10 @@
 
 #include <inttypes.h>
 
+#define CONCAT_(prefix, suffix) prefix##suffix
+#define CONCAT(prefix, suffix) CONCAT_(prefix, suffix)
+#define UNIQUE_VAR(prefix) CONCAT(prefix##_, __LINE__)
+
 namespace utils
 {
     void divmod10(uint32_t in, uint32_t &div, uint8_t &mod) __attribute__((noinline));
